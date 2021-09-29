8fa43883093264fc15bdae2eaf8ad0ddf10770e6
Sebastien Corbin <seb.corbin@gmail.com>
2021-09-29 11:39:16 +0200

    Detail tab

--- templatetags/admission.py
+++ templatetags/admission.py
@@ -23,12 +23,49 @@
 #    see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
+import functools
+from inspect import getfullargspec
+
 from django import template
-from django.views.generic import DetailView
+from django.views.generic import FormView
 
 from admission.utils import admission_profile_valid_tabs
 
 register = template.Library()
+
+
+class InclusionWithBodyNode(template.library.InclusionNode):
+    def __init__(self, nodelist, func, takes_context, args, kwargs, filename, context_name):
+        super().__init__(func, takes_context, args, kwargs, filename)
+        self.nodelist = nodelist
+        self.context_name = context_name
+
+    def render(self, context):
+        context[self.context_name] = self.nodelist.render(context)
+        return super().render(context)
+
+
+def register_inclusion_with_body(filename, takes_context=None, name=None, context_name='body'):
+    def dec(func):
+        params, varargs, varkw, defaults, kwonly, kwonly_defaults, _ = getfullargspec(func)
+        function_name = name or getattr(func, '_decorated_function', func).__name__
+
+        @functools.wraps(func)
+        def compile_func(parser, token):
+            bits = token.split_contents()[1:]
+            args, kwargs = template.library.parse_bits(
+                parser, bits, params, varargs, varkw, defaults,
+                kwonly, kwonly_defaults, takes_context, function_name,
+            )
+            nodelist = parser.parse((f'end{function_name}',))
+            parser.delete_first_token()
+            return InclusionWithBodyNode(
+                nodelist, func, takes_context, args, kwargs, filename, context_name
+            )
+
+        register.tag(function_name, compile_func)
+        return func
+    return dec
 
 
 @register.inclusion_tag('admission/doctorate_tabs_bar.html', takes_context=True)
@@ -40,14 +77,36 @@ def doctorate_tabs(context, admission=None):
     return {
         'valid_tabs': valid_tabs,
         'admission': admission,
-        'detail_view': isinstance(context['view'], DetailView),
+        'detail_view': not isinstance(context['view'], FormView),
+        'admission_uuid': context['view'].kwargs.get('pk', ''),
     }
+
+
+@register.inclusion_tag('admission/document_list.html')
+def document_list(files):
+    return {'files': files}
 
 
 @register.inclusion_tag('admission/field_data.html')
 def field_data(name, data=None, css_class=None):
+    if isinstance(data, list) and data:
+        template_string = "{% load admission %}{% document_list files %}"
+        template_context = {'files': data}
+        data = template.Template(template_string).render(template.Context(template_context))
     return {
         'name': name,
         'data': data,
         'css_class': css_class,
     }
+
+
+@register_inclusion_with_body('panel.html', takes_context=True, context_name='panel_body')
+def panel(context, title=''):
+    """
+    Template tag for panel
+    :param title: the panel title
+    :type context: django.template.context.RequestContext
+    """
+    context.setdefault('faq_counter', 0)
+    context['title'] = title
+    return context
