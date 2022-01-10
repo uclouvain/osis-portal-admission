3913b03690926342cfeb8babac1f38ec6a7c19e2
Julien Cougnaud <jco@makina-corpus.net>
2022-01-10 11:34:15 +0100

    Change the tests to be compatible with the right python version

--- tests/views/test_supervision.py
+++ tests/views/test_supervision.py
@@ -23,7 +23,7 @@
 #    see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, ANY
 
 from django.shortcuts import resolve_url
 from django.test import TestCase
@@ -46,6 +46,13 @@ class SupervisionTestCase(TestCase):
 
         self.client.force_login(self.person.user)
         self.url = resolve_url("admission:doctorate-update:supervision", pk=self.pk)
+        self.default_kwargs = {
+            'accept_language': ANY,
+            'x_user_first_name': ANY,
+            'x_user_last_name': ANY,
+            'x_user_email': ANY,
+            'x_user_global_id': ANY,
+        }
 
         api_patcher = patch("osis_admission_sdk.api.propositions_api.PropositionsApi")
         self.mock_api = api_patcher.start()
@@ -177,18 +184,17 @@ class SupervisionTestCase(TestCase):
             'comment': "The public comment",
             'rejection_reason': "The reason",  # The reason is provided but will not be used
         })
-        self.mock_api.return_value.approve_proposition.assert_called()
-
-        kwargs = self.mock_api.return_value.approve_proposition.call_args.kwargs
 
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-
-        self.assertEqual(kwargs.get('uuid', None), self.pk)
-        self.assertEqual(kwargs.get('approuver_proposition_command', None), {
-            'commentaire_interne': "The external comment",
-            'commentaire_externe': "The public comment",
-            'matricule': self.person.global_id,
-        })
+        self.mock_api.return_value.approve_proposition.assert_called_with(
+            uuid=self.pk,
+            approuver_proposition_command={
+                'commentaire_interne': "The external comment",
+                'commentaire_externe': "The public comment",
+                'matricule': self.person.global_id,
+            },
+            **self.default_kwargs,
+        )
 
         self.mock_api.return_value.approve_proposition.reset_mock()
 
@@ -199,25 +205,24 @@ class SupervisionTestCase(TestCase):
             'comment': "The public comment",
             'rejection_reason': "The reason",
         })
-        self.mock_api.return_value.reject_proposition.assert_called()
-
-        kwargs = self.mock_api.return_value.reject_proposition.call_args.kwargs
 
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
 
-        self.assertEqual(kwargs.get('uuid', None), self.pk)
-        self.assertEqual(kwargs.get('refuser_proposition_command', None), {
-            'commentaire_interne': "The external comment",
-            'commentaire_externe': "The public comment",
-            'motif_refus': "The reason",
-            'matricule': self.person.global_id,
-        })
+        self.mock_api.return_value.reject_proposition.assert_called_with(
+            uuid=self.pk,
+            refuser_proposition_command={
+                'commentaire_interne': "The external comment",
+                'commentaire_externe': "The public comment",
+                'motif_refus': "The reason",
+                'matricule': self.person.global_id,
+            },
+            **self.default_kwargs,
+        )
 
         self.mock_api.return_value.reject_proposition.reset_mock()
 
         # The decision is missing
         response = self.client.post(url, {
-            # 'decision': '',
             'internal_comment': "The external comment",
             'comment': "The public comment",
             'rejection_reason': "The reason",
