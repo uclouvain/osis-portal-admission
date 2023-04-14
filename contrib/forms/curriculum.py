501edcee1717bb0f282d7a196363a75b78aaaea5
Sebastien Corbin <seb.corbin@gmail.com>
2023-04-14 10:37:58 +0200

    OS-445 Fix error start_date greater than end_date

--- contrib/forms/curriculum.py
+++ contrib/forms/curriculum.py
@@ -1,28 +1,29 @@
 # ##############################################################################
 #
-#     OSIS stands for Open Student Information System. It's an application
-#     designed to manage the core business of higher education institutions,
-#     such as universities, faculties, institutes and professional schools.
-#     The core business involves the administration of students, teachers,
-#     courses, programs and so on.
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
 #
-#     Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
+#  Copyright (C) 2015-2023 Université catholique de Louvain (http://www.uclouvain.be)
 #
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#     A copy of this license - GNU General Public License - is available
-#     at the root of the source code of this program.  If not,
-#     see http://www.gnu.org/licenses/.
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
 #
 # ##############################################################################
+
 from copy import deepcopy
 from datetime import datetime
 from functools import partial
@@ -244,7 +245,7 @@ class AdmissionCurriculumProfessionalExperienceForm(forms.Form):
             if start_date_year > end_date_year or (
                 start_date_year == end_date_year and start_date_month > end_date_month
             ):
-                self.add_error(None, _("The start date must be equals or lower than the end date."))
+                self.add_error(None, _("The start date must be equal or lower than the end date."))
 
         activity_type = cleaned_data.get('type')
 
@@ -548,7 +549,7 @@ class AdmissionCurriculumEducationalExperienceForm(ByContextAdmissionForm):
         start = cleaned_data.get('start')
         end = cleaned_data.get('end')
         if start and end and int(start) > int(end):
-            self.add_error(None, _("The start date must be equals or lower than the end date."))
+            self.add_error(None, _("The start date must be equal or lower than the end date."))
 
         # Institute fields
         self.clean_data_institute(cleaned_data)
