3303f4404d53dd3cb5925ce713c1e489931d652e
Gagaro <gagaro42@gmail.com>
2023-11-21 12:52:00 +0100

    Fix OS-658: Add tooltip for id photo

--- contrib/forms/person.py
+++ contrib/forms/person.py
@@ -212,6 +212,16 @@ class DoctorateAdmissionPersonForm(forms.Form):
     id_photo = FileUploadField(
         required=False,
         label=_("Identification photo"),
+        help_text=_(
+            "The photo must follow the following rules:"
+            "<ul>"
+            "  <li>JPEG or PNG file (.jpg, .jpeg ou .png) up to 10MB</li>"
+            "  <li>Identity photo format with a blank background</li>"
+            "  <li>Centered on your bare face</li>"
+            "  <li>Recent and in colour</li>"
+            "  <li>Without any filter nor any contour</li>"
+            "</ul>"
+        ),
         max_files=1,
         mimetypes=IMAGE_MIME_TYPES,
     )
