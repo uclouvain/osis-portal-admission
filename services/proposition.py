740a86ef552d7cbc17340bf86e55cef6507cdca8
Gagaro <gagaro42@gmail.com>
2025-12-01 16:33:16 +0100

    Fix OS-1651 OS-1653 OS-1654 OS-1655: Contingenté front-office

--- services/proposition.py
+++ services/proposition.py
@@ -655,6 +655,7 @@ class FormationGeneraleBusinessException(Enum):
     InformationsVisaNonCompleteesException = "FORMATION-GENERALE-20"
     BoursesEtudesNonRenseignees = "FORMATION-GENERALE-36"
     ExamenNonCompletesException = "FORMATION-GENERALE-39"
+    UneSeuleFormationContingentePossible = "FORMATION-GENERALE-42"
 
 
 class FormationContinueBusinessException(Enum):
@@ -743,6 +744,7 @@ BUSINESS_EXCEPTIONS_BY_TAB = {
         GlobalPropositionBusinessException.AucunPoolCorrespondantException,
         GlobalPropositionBusinessException.NombrePropositionsSoumisesDepasseException,
         DoctorateBusinessException.MaximumPropositionsDoctoralesAtteintException,
+        FormationGeneraleBusinessException.UneSeuleFormationContingentePossible,
     },
     'confirmation-paper': set(),
     'extension-request': set(),
