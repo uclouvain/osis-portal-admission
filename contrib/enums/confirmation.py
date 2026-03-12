13d74f4abd05ab0a576b56265129ae7e8474b198
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2026-03-12 18:07:44 +0100

    [OS-1754] Re-enrolment > confirmation tab

---
+++ contrib/enums/confirmation.py
@@ -0,0 +1,52 @@
+# ##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2026 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+# ##############################################################################
+
+from django.utils.translation import gettext_lazy as _
+
+from base.models.utils.utils import ChoiceEnum
+
+
+class RaisonPlusieursDemandesMemesCycleEtAnnee(ChoiceEnum):
+    ANNULER_PRECEDENTES_DEMANDES = _('Create this new application and cancel your other ones')
+    SUIVRE_EN_PARALLELE = _('Take several courses at the same time')
+
+    @classmethod
+    def choices_with_training_name(cls, training):
+        return (
+            (
+                cls.ANNULER_PRECEDENTES_DEMANDES.name,
+                _(
+                    'Create an application for %(training_name)s (%(training_campus)s) '
+                    '%(training_acronym)s and cancel your other applications'
+                )
+                % {
+                    'training_name': training.intitule or training.intitule_fr,
+                    'training_campus': training.campus,
+                    'training_acronym': training.sigle,
+                },
+            ),
+            (cls.SUIVRE_EN_PARALLELE.name, cls.SUIVRE_EN_PARALLELE.value),
+        )
--- tests/views/test_confirm_submit.py
+++ tests/views/test_confirm_submit.py
@@ -32,9 +32,15 @@ from django.shortcuts import resolve_url
 from django.test import override_settings
 from django.utils.translation import gettext_lazy as _
 from osis_admission_sdk.model.pool_enum import PoolEnum
+from osis_admission_sdk.model.specifier_raison_plusieurs_demandes_meme_cycle_meme_annee_command import (
+    SpecifierRaisonPlusieursDemandesMemeCycleMemeAnneeCommand,
+)
+from osis_admission_sdk.model.submit_general_proposition import SubmitGeneralProposition
 from osis_admission_sdk.model.submit_proposition import SubmitProposition
 
+from admission.constants import FIELD_REQUIRED_MESSAGE
 from admission.contrib.enums import ChoixStatutPropositionGenerale
+from admission.contrib.enums.confirmation import RaisonPlusieursDemandesMemesCycleEtAnnee
 from base.tests.factories.person import PersonFactory
 from base.tests.test_case import OsisPortalTestCase
 from frontoffice.settings.osis_sdk.utils import (
@@ -331,7 +337,7 @@ class ConfirmSubmitTestCase(OsisPortalTestCase):
         self.assertRedirects(response, url)
         api.submit_general_education_proposition.assert_called_with(
             uuid=uuid,
-            submit_proposition=SubmitProposition(
+            submit_general_proposition=SubmitGeneralProposition(
                 **{
                     'pool': PoolEnum(value='ADMISSION_POOL_UE5_BELGIAN'),
                     'annee': 2020,
@@ -341,6 +347,8 @@ class ConfirmSubmitTestCase(OsisPortalTestCase):
                         'declaration_sur_lhonneur': "<ul><li>Element1</li></ul>",
                         'justificatifs': 'I understand',
                     },
+                    'raison_plusieurs_demandes_meme_cycle_meme_annee': '',
+                    'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': '',
                 }
             ),
             **self.default_kwargs,
@@ -363,7 +371,7 @@ class ConfirmSubmitTestCase(OsisPortalTestCase):
         self.assertRedirects(response, url, fetch_redirect_response=False)
         api.submit_general_education_proposition.assert_called_with(
             uuid=uuid,
-            submit_proposition=SubmitProposition(
+            submit_general_proposition=SubmitGeneralProposition(
                 **{
                     'pool': PoolEnum(value='ADMISSION_POOL_UE5_BELGIAN'),
                     'annee': 2020,
@@ -373,7 +381,181 @@ class ConfirmSubmitTestCase(OsisPortalTestCase):
                         'declaration_sur_lhonneur': "<ul><li>Element1</li></ul>",
                         'justificatifs': 'I understand',
                     },
+                    'raison_plusieurs_demandes_meme_cycle_meme_annee': '',
+                    'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': '',
                 }
             ),
             **self.default_kwargs,
         )
+
+    def test_post_general_with_questions_related_to_several_applications_for_the_same_cycle_and_the_same_year(self):
+        api = self.mock_proposition_api.return_value
+
+        uuid = "3c5cdc60-2537-4a12-a396-64d2e9e34876"
+
+        url = resolve_url('admission:general-education:update:confirm-submit', pk=uuid)
+
+        verification = api.verify_general_education_proposition.return_value.to_dict
+        verification.return_value = api.verify_proposition.return_value.to_dict.return_value
+        api.submit_general_education_proposition.return_value.to_dict.return_value = {
+            'status': ChoixStatutPropositionGenerale.CONFIRMEE.name,
+            'uuid': uuid,
+        }
+
+        # The specific questions are not displayed
+        verification.return_value['display_several_applications_same_cycle_same_year_questions'] = False
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['confirmation_form']
+
+        self.assertIsNone(form.fields.get('raison_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNone(form.fields.get('justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNotNone(form.fields.get('foo'))
+        self.assertIsNotNone(form.fields.get('bar'))
+
+        # The specific questions are displayed
+        verification.return_value['display_several_applications_same_cycle_same_year_questions'] = True
+
+        # > The answers to the questions are not completed when the form is loaded so only these questions are displayed
+        general_proposition = api.retrieve_general_education_proposition
+        general_proposition.return_value.raison_plusieurs_demandes_meme_cycle_meme_annee = ''
+        general_proposition.return_value.justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee = ''
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['confirmation_form']
+
+        self.assertIsNotNone(form.fields.get('raison_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNotNone(form.fields.get('justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNone(form.fields.get('foo'))
+        self.assertIsNone(form.fields.get('bar'))
+
+        response = self.client.post(
+            url,
+            data={
+                'pool': 'ADMISSION_POOL_UE5_BELGIAN',
+                'annee': 2020,
+                'multiple-applications-form': '1',
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['confirmation_form']
+
+        self.assertFalse(form.is_valid())
+
+        self.assertIn(FIELD_REQUIRED_MESSAGE, form.errors.get('raison_plusieurs_demandes_meme_cycle_meme_annee', []))
+        self.assertIn(
+            FIELD_REQUIRED_MESSAGE,
+            form.errors.get('justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee', []),
+        )
+
+        response = self.client.post(
+            url,
+            data={
+                'pool': 'ADMISSION_POOL_UE5_BELGIAN',
+                'annee': 2020,
+                'raison_plusieurs_demandes_meme_cycle_meme_annee': (
+                    RaisonPlusieursDemandesMemesCycleEtAnnee.ANNULER_PRECEDENTES_DEMANDES.name
+                ),
+                'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': 'Reason',
+                'multiple-applications-form': '1',
+            },
+        )
+
+        self.assertRedirects(response=response, expected_url=url)
+
+        api.specify_reason_multiple_applications_same_cycle_same_year.assert_called_with(
+            uuid=uuid,
+            specifier_raison_plusieurs_demandes_meme_cycle_meme_annee_command=(
+                SpecifierRaisonPlusieursDemandesMemeCycleMemeAnneeCommand(
+                    **{
+                        'raison_plusieurs_demandes_meme_cycle_meme_annee': (
+                            RaisonPlusieursDemandesMemesCycleEtAnnee.ANNULER_PRECEDENTES_DEMANDES.name
+                        ),
+                        'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': 'Reason',
+                    }
+                )
+            ),
+            **self.default_kwargs,
+        )
+
+        # The answers to the questions are completed when the form is loaded
+        general_proposition.return_value.raison_plusieurs_demandes_meme_cycle_meme_annee = (
+            RaisonPlusieursDemandesMemesCycleEtAnnee.ANNULER_PRECEDENTES_DEMANDES.name
+        )
+        general_proposition.return_value.justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee = 'Reason'
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['confirmation_form']
+
+        self.assertIsNotNone(form.fields.get('raison_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNotNone(form.fields.get('justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee'))
+        self.assertIsNotNone(form.fields.get('foo'))
+        self.assertIsNotNone(form.fields.get('bar'))
+
+        response = self.client.post(
+            url,
+            data={
+                **self.data_ok,
+                'pool': 'ADMISSION_POOL_UE5_BELGIAN',
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        form = response.context['confirmation_form']
+
+        self.assertFalse(form.is_valid())
+
+        self.assertIn(FIELD_REQUIRED_MESSAGE, form.errors.get('raison_plusieurs_demandes_meme_cycle_meme_annee', []))
+        self.assertIn(
+            FIELD_REQUIRED_MESSAGE,
+            form.errors.get('justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee', []),
+        )
+
+        response = self.client.post(
+            url,
+            data={
+                **self.data_ok,
+                'pool': 'ADMISSION_POOL_UE5_BELGIAN',
+                'raison_plusieurs_demandes_meme_cycle_meme_annee': (
+                    RaisonPlusieursDemandesMemesCycleEtAnnee.SUIVRE_EN_PARALLELE.name
+                ),
+                'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': 'Parallel reason',
+            },
+            follow=True,
+        )
+
+        self.assertRedirects(response=response, expected_url=resolve_url('admission:list'))
+
+        api.submit_general_education_proposition.assert_called_with(
+            uuid=uuid,
+            submit_general_proposition=SubmitGeneralProposition(
+                **{
+                    'pool': PoolEnum(value='ADMISSION_POOL_UE5_BELGIAN'),
+                    'annee': 2020,
+                    'elements_confirmation': {
+                        'foo': "I allow Test",
+                        'bar': "I do not authorize Test to do something with my data",
+                        'declaration_sur_lhonneur': "<ul><li>Element1</li></ul>",
+                        'justificatifs': 'I understand',
+                    },
+                    'raison_plusieurs_demandes_meme_cycle_meme_annee': (
+                        RaisonPlusieursDemandesMemesCycleEtAnnee.SUIVRE_EN_PARALLELE.name
+                    ),
+                    'justification_textuelle_plusieurs_demandes_meme_cycle_meme_annee': 'Parallel reason',
+                }
+            ),
+            **self.default_kwargs,
+        )
+        self.assertContains(response, _("Your application has been submitted"))
